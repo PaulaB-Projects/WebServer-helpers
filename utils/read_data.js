0b0f2a4ed5657c105a3c404778bcfa1f775ce9f1
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-02-18 20:11:05 +0000

    slight change to read_data.js, passing the path as parameter

--- utils/read_data.js
+++ utils/read_data.js
@@ -1,7 +1,6 @@
 import { readFileSync as read } from 'node:fs';
 
 const ENCODING = 'utf8';
-const PATH = './data/'
 
-export default file_name =>
-	JSON.parse(read(`${ PATH }${ file_name }.json`, ENCODING));
+export default (file_name, path ) =>
+	JSON.parse(read(`${ path }${ file_name }.json`, ENCODING));
